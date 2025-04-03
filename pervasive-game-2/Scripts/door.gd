71c8cfed135c525d89dfa7b33ac9b6bd0617726a
ShannynHH <163212430+ShannynHH@users.noreply.github.com>
2025-04-03 13:57:31 +1100

    Opening/Closing Doors
    Added doors that open and close when the player leaves a specific zone in front of them

---
+++ pervasive-game-2/Scripts/door.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player"):
+		get_node("AnimationPlayer").play("Door_Open")
+
+func _on_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player"):
+		get_node("AnimationPlayer").play("Door_Closed")
