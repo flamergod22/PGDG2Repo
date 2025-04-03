0a5c38e763fcd2f0061abd4972cd6623ad8b9c56
ShannynHH <163212430+ShannynHH@users.noreply.github.com>
2025-04-03 23:09:51 +1100

    Added Dud Doors
    Changed some folder names to better describe contents
    Added Dud Doors that dont open and play a sound effect instead

--- pervasive-game-2/Scripts/door.gd
+++ pervasive-game-2/Scripts/door.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-
+#this is good door script, for opening + flashback
 
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
