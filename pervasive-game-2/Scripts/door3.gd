72a622f7a6f076bf32b854cde0c546880473662c
ShannynHH <163212430+ShannynHH@users.noreply.github.com>
2025-04-04 20:19:23 +1100

    Added Flashback Mechanic

--- pervasive-game-2/Scripts/door3.gd
+++ pervasive-game-2/Scripts/door3.gd
@@ -5,7 +5,7 @@ extends Area3D
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
 		get_node("AnimationPlayer").play("Door_Open")
-		get_node("FlashbackPlayer1").play ("flashback1")
+		get_node("FlashbackPlayer3").play ("flashback3")
 
 func _on_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
